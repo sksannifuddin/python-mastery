acf62f704486f999bb064b6a305646885d8e79ad
sksannifuddin <sksannifuddin04@gmail.com>
2026-07-07 12:04:06 +0530

    update functions_1.py started

---
+++ DAY-5/functions_1.py
@@ -0,0 +1,176 @@
+# 📘 FUNCTIONS — MODULE 1
+# 30 Important Coding Questions
+# Q1
+
+# Create a function that prints:
+
+# Hello Python
+# Q2
+
+# Create a function that prints your name.
+
+# Q3
+
+# Call the same function three times.
+
+# Q4
+
+# Create a function that accepts one parameter (name) and prints:
+
+# Hello Sunny
+# Q5
+
+# Create a function that accepts two numbers and prints their sum.
+
+# Q6
+
+# Create a function that accepts three numbers and prints the largest.
+
+# Q7
+
+# Create a function that accepts a number and prints whether it is even or odd.
+
+# Q8
+
+# Create a function that accepts a number and prints its multiplication table.
+
+# Q9
+
+# Create a function that accepts a string and prints it in uppercase.
+
+# Q10
+
+# Create a function that accepts a list and prints every element.
+
+# Q11
+
+# Create a function that returns the square of a number.
+
+# Q12
+
+# Create a function that returns the cube of a number.
+
+# Q13
+
+# Create a function that returns the largest of two numbers.
+
+# Q14
+
+# Create a function that returns the smallest of three numbers.
+
+# Q15
+
+# Create a function that returns both:
+
+# Sum
+# Product
+
+# using a single return.
+
+# Q16
+
+# Call the above function and unpack the returned values.
+
+# Q17
+
+# Write a function with a default parameter.
+
+# Example:
+
+# def greet(name="Guest"):
+# Q18
+
+# Call the function:
+
+# without argument
+# with argument
+
+# Observe the output.
+
+# Q19
+
+# Create a function using keyword arguments.
+
+# Example:
+
+# student(name="Sunny", age=21)
+# Q20
+
+# Create a function having four parameters.
+
+# Call it using keyword arguments in a different order.
+
+# Q21
+
+# Create a function using *args.
+
+# Print all values.
+
+# Q22
+
+# Using *args, find the total without using sum().
+
+# Q23
+
+# Using *args, find the largest number.
+
+# Q24
+
+# Write a function that returns None.
+
+# Print the returned value.
+
+# Q25
+
+# Show the difference between:
+
+# print()
+
+# and
+
+# return
+# Q26
+
+# Demonstrate local variables.
+
+# Q27
+
+# Demonstrate global variables.
+
+# Q28
+
+# Use the global keyword to modify a global variable.
+
+# Q29
+
+# Write a function with a docstring.
+
+# Print the docstring using:
+
+# print(function_name.__doc__)
+# Q30 ⭐ Google/Amazon Mini Challenge
+
+# Without using built-in functions like sum(), max(), or min(), write one function that accepts any number of integers using *args and returns:
+
+# Total
+# Average
+# Largest
+# Smallest
+# Even count
+# Odd count
+
+# Example:
+
+# result = analyze(10,20,30,40,50)
+
+# print(result)
+
+# Expected Output:
+
+# Total = 150
+# Average = 30
+# Largest = 50
+# Smallest = 10
+# Even Count = 5
+# Odd Count = 0
+
